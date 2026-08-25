78d9312109b588b456ed7547099db731ce3d4d4e
j.tide <iamzjt@foxmail.com>
2026-08-25 16:49:22 +0800

    feat(api): 加载应用配置

--- llmops-api/internal/server/http.py
+++ llmops-api/internal/server/http.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
+from config import Config
 from internal.router import Router
 
 
 class Http(Flask):
     """Http服务引擎"""
 
-    def __init__(self, *args, router: Router, **kwargs):
+    def __init__(self, *args, conf: Config, router: Router, **kwargs):
         super().__init__(*args, **kwargs)
+        self.config.from_object(conf)
         # 注册应用路由
         router.register(self)
