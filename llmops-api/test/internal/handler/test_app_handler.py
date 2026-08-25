12c925a3b65f61ba3bee2820a1cc38e8dacf45eb
j.tide <iamzjt@foxmail.com>
2026-08-25 23:05:12 +0800

    test(api): 完善聊天补全接口参数化测试

--- llmops-api/test/internal/handler/test_app_handler.py
+++ llmops-api/test/internal/handler/test_app_handler.py
@@ -1,5 +1,17 @@
+import pytest
+
+from pkg.response import HttpCode
+
+
 class TestAppHandler:
   """app控制器的测试类"""
 
-  def test_completion(self, client):
-    resp = client.post('/app/completion', json={'query': '你好，你是？'})
+  @pytest.mark.parametrize('query', '你好，你是？')
+  def test_completion(self, query, client):
+    resp = client.post('/app/completion', json={'query': query})
+    assert resp.status_code == 200
+    if query is None:
+      assert resp.json.get('code') == HttpCode.VALIDATE_ERROR
+    else:
+      assert resp.json.get('code') == HttpCode.SUCCESS
+    print('响应内容：', resp.json)
