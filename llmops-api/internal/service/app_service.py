126d6f7a7fdb1ab7c026848facb567b2bb0dd851
j.tide <iamzjt@foxmail.com>
2026-08-28 15:04:51 +0800

    refactor(api): 统一应用写操作事务管理

--- llmops-api/internal/service/app_service.py
+++ llmops-api/internal/service/app_service.py
@@ -15,19 +15,18 @@ class AppService:
   db: SQLAlchemy
 
   def create_app(self) -> App:
-    # 1.创建模型的实体类
-    app = App(
-      name='测试机器人',
-      account_id=uuid.uuid4(),
-      icon='',
-      description='这是一个简单的聊天机器人',
-    )
+    with self.db.auto_commit():
+      # 1.创建模型的实体类
+      app = App(
+        name='测试机器人',
+        account_id=uuid.uuid4(),
+        icon='',
+        description='这是一个简单的聊天机器人',
+      )
 
-    # 2.将实体类添加到session会话中
-    self.db.session.add(app)
+      # 2.将实体类添加到session会话中
+      self.db.session.add(app)
 
-    # 3.提交session会话
-    self.db.session.commit()
     return app
 
   def get_app(self, id: uuid.UUID) -> App:
@@ -35,13 +34,13 @@ class AppService:
     return app
 
   def update_app(self, id: uuid.UUID) -> App:
-    app = self.get_app(id)
-    app.name = '小Z助手'
-    self.db.session.commit()
+    with self.db.auto_commit():
+      app = self.get_app(id)
+      app.name = '小Z助手'
     return app
 
   def delete_app(self, id: uuid.UUID) -> App:
-    app = self.get_app(id)
-    self.db.session.delete(app)
-    self.db.session.commit()
+    with self.db.auto_commit():
+      app = self.get_app(id)
+      self.db.session.delete(app)
     return app
