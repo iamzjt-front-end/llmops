7dc0c0f3dfbfde93dbff28921ac14f59f2e11424
j.tide <iamzjt@foxmail.com>
2026-09-03 22:37:03 +0800

    feat(study): 添加实体记忆组件示例

---
+++ llmops-api/study/12-LangChain实体记忆组件的使用与解析/1.实体记忆组件示例.py
@@ -0,0 +1,24 @@
+import dotenv
+from langchain_classic.chains.conversation.base import ConversationChain
+from langchain_classic.memory import ConversationEntityMemory
+from langchain_classic.memory.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
+from langchain_community.chat_models.baidu_qianfan_endpoint import QianfanChatEndpoint
+
+dotenv.load_dotenv()
+
+# llm = ChatOpenAI(model="gpt-4o", temperature=0)
+llm = QianfanChatEndpoint()
+
+chain = ConversationChain(
+  llm=llm,
+  prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
+  memory=ConversationEntityMemory(llm=llm),
+)
+
+print(chain.invoke({'input': '你好，我是慕小课。我最近正在学习LangChain。'}))
+print(chain.invoke({'input': '我最喜欢的编程语言是 Python。'}))
+print(chain.invoke({'input': '我住在广州'}))
+
+# 查询实体中的对话
+res = chain.memory.entity_store.store
+print(res)
