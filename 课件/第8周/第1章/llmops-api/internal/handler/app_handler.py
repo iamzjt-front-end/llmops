285f30a35864fe6ac3967a0c933add997179055c
j.tide <iamzjt@foxmail.com>
2026-09-07 12:31:47 +0800

    docs(课件): 添加第8周第1章Agent智能体对话模块课程资料

---
+++ 课件/第8周/第1章/llmops-api/internal/handler/app_handler.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2024/3/29 14:59
+@Author  : thezehui@gmail.com
+@File    : app_handler.py
+"""
+import json
+import uuid
+from dataclasses import dataclass
+from typing import Any, Dict, Generator
+from uuid import UUID
+
+from injector import inject
+from langchain_core.documents import Document
+from langchain_core.memory import BaseMemory
+from langchain_core.runnables import RunnableConfig
+from langchain_core.tracers import Run
+from langchain_openai import ChatOpenAI
+from redis import Redis
+
+from internal.core.agent.agents import FunctionCallAgent, AgentQueueManager
+from internal.core.agent.entities.agent_entity import AgentConfig
+from internal.core.tools.builtin_tools.providers import BuiltinProviderManager
+from internal.entity.conversation_entity import InvokeFrom
+from internal.schema.app_schema import CompletionReq
+from internal.service import AppService, VectorDatabaseService, ApiToolService, EmbeddingsService, ConversationService
+from pkg.response import success_json, validate_error_json, success_message, compact_generate_response
+
+
+@inject
+@dataclass
+class AppHandler:
+    """应用控制器"""
+    app_service: AppService
+    vector_database_service: VectorDatabaseService
+    api_tool_service: ApiToolService
+    embeddings_service: EmbeddingsService
+    builtin_provider_manager: BuiltinProviderManager
+    conversation_service: ConversationService
+    redis_client: Redis
+
+    def create_app(self):
+        """调用服务创建新的APP记录"""
+        app = self.app_service.create_app()
+        return success_message(f"应用已经成功创建，id为{app.id}")
+
+    def get_app(self, id: uuid.UUID):
+        app = self.app_service.get_app(id)
+        return success_message(f"应用已经成功获取，名字是{app.name}")
+
+    def update_app(self, id: uuid.UUID):
+        app = self.app_service.update_app(id)
+        return success_message(f"应用已经成功修改，修改的名字是:{app.name}")
+
+    def delete_app(self, id: uuid.UUID):
+        app = self.app_service.delete_app(id)
+        return success_message(f"应用已经成功删除，id为:{app.id}")
+
+    @classmethod
+    def _load_memory_variables(cls, input: Dict[str, Any], config: RunnableConfig) -> Dict[str, Any]:
+        """加载记忆变量信息"""
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            return configurable_memory.load_memory_variables(input)
+        return {"history": []}
+
+    @classmethod
+    def _save_context(cls, run_obj: Run, config: RunnableConfig) -> None:
+        """存储对应的上下文信息到记忆实体中"""
+        configurable = config.get("configurable", {})
+        configurable_memory = configurable.get("memory", None)
+        if configurable_memory is not None and isinstance(configurable_memory, BaseMemory):
+            configurable_memory.save_context(run_obj.inputs, run_obj.outputs)
+
+    def debug(self, app_id: UUID):
+        """应用会话调试聊天接口，该接口为流式事件输出"""
+        # 1.提取从接口中获取的输入，POST
+        req = CompletionReq()
+        if not req.validate():
+            return validate_error_json(req.errors)
+
+        # 2.定义工具列表
+        tools = [
+            self.builtin_provider_manager.get_tool("google", "google_serper")(),
+            self.builtin_provider_manager.get_tool("gaode", "gaode_weather")(),
+            self.builtin_provider_manager.get_tool("dalle", "dalle3")(),
+        ]
+
+        agent = FunctionCallAgent(
+            AgentConfig(
+                llm=ChatOpenAI(model="gpt-4o-mini"),
+                enable_long_term_memory=True,
+                tools=tools,
+            ),
+            AgentQueueManager(
+                user_id=uuid.uuid4(),
+                task_id=uuid.uuid4(),
+                invoke_from=InvokeFrom.DEBUGGER,
+                redis_client=self.redis_client,
+            )
+        )
+
+        def stream_event_response() -> Generator:
+            """流式事件输出响应"""
+            for agent_queue_event in agent.run(req.query.data, [], "用户介绍自己叫慕小课"):
+                data = {
+                    "id": str(agent_queue_event.id),
+                    "task_id": str(agent_queue_event.task_id),
+                    "event": agent_queue_event.event,
+                    "thought": agent_queue_event.thought,
+                    "observation": agent_queue_event.observation,
+                    "tool": agent_queue_event.tool,
+                    "tool_input": agent_queue_event.tool_input,
+                    "answer": agent_queue_event.answer,
+                    "latency": agent_queue_event.latency
+                }
+                yield f"event: {agent_queue_event.event}\ndata: {json.dumps(data)}\n\n"
+
+        return compact_generate_response(stream_event_response())
+
+    @classmethod
+    def _combine_documents(cls, documents: list[Document]) -> str:
+        """将传入的文档列表合并成字符串"""
+        return "\n\n".join([document.page_content for document in documents])
+
+    def ping(self):
+        from internal.core.agent.agents import FunctionCallAgent
+        from internal.core.agent.entities.agent_entity import AgentConfig
+        from langchain_openai import ChatOpenAI
+
+        agent = FunctionCallAgent(AgentConfig(
+            llm=ChatOpenAI(model="gpt-4o-mini"),
+            preset_prompt="你是一个拥有20年经验的诗人，请根据用户提供的主题来写一首诗"
+        ))
+        state = agent.run("程序员", [], "")
+        content = state["messages"][-1].content
+
+        return success_json({"content": content})
---
+++ 课件/第8周/第1章/llmops-ui/src/utils/request.ts
@@ -0,0 +1,174 @@
+import { Message } from '@arco-design/web-vue'
+import { apiPrefix, httpCode } from '@/config'
+
+// 1.超时时间为100s
+const TIME_OUT = 100000
+
+// 2.基础的配置
+const baseFetchOptions = {
+  method: 'GET',
+  mode: 'cors',
+  credentials: 'include',
+  headers: new Headers({
+    'Content-Type': 'application/json',
+  }),
+  redirect: 'follow',
+}
+
+// 3.fetch参数类型
+type FetchOptionType = Omit<RequestInit, 'body'> & {
+  params?: Record<string, any>
+  body?: BodyInit | Record<string, any> | null
+}
+
+// 4.封装基础的fetch请求
+const baseFetch = <T>(url: string, fetchOptions: FetchOptionType): Promise<T> => {
+  // 5.将所有的配置信息合并起来
+  const options: typeof baseFetchOptions & FetchOptionType = Object.assign(
+    {},
+    baseFetchOptions,
+    fetchOptions,
+  )
+
+  // 6.组装url
+  let urlWithPrefix = `${apiPrefix}${url.startsWith('/') ? url : `/${url}`}`
+
+  // 7.解构出对应的请求方法、params、body参数
+  const { method, params, body } = options
+
+  // 8.如果请求是GET方法，并且传递了params参数
+  if (method === 'GET' && params) {
+    const paramsArray: string[] = []
+    Object.keys(params).forEach((key) => {
+      paramsArray.push(`${key}=${encodeURIComponent(params[key])}`)
+    })
+    if (urlWithPrefix.search(/\?/) === -1) {
+      urlWithPrefix += `?${paramsArray.join('&')}`
+    } else {
+      urlWithPrefix += `&${paramsArray.join('&')}`
+    }
+
+    delete options.params
+  }
+
+  // 9.处理post传递的数据
+  if (body) {
+    options.body = JSON.stringify(body)
+  }
+
+  // 10.同时发起两个Promise(或者是说两个操作，看谁先返回，就先结束)
+  return Promise.race([
+    // 11.使用定时器来检测是否超时
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject('接口已超时')
+      }, TIME_OUT)
+    }),
+    // 12.发起一个正常请求
+    new Promise((resolve, reject) => {
+      globalThis
+        .fetch(urlWithPrefix, options as RequestInit)
+        .then(async (res) => {
+          const json = await res.json()
+          if (json.code === httpCode.success) {
+            resolve(json)
+          } else {
+            Message.error(json.message)
+            reject(new Error(json.message))
+          }
+        })
+        .catch((err) => {
+          Message.error(err.message)
+          reject(err)
+        })
+    }),
+  ]) as Promise<T>
+}
+
+// 5.封装基于post的sse(流式事件响应)请求
+export const ssePost = async (
+  url: string,
+  fetchOptions: FetchOptionType,
+  onData: (data: { [key: string]: any }) => void,
+) => {
+  // 5.1 组装基础的fetch请求配置
+  const options = Object.assign({}, baseFetchOptions, { method: 'POST' }, fetchOptions)
+
+  // 5.2 组装请求URL
+  const urlWithPrefix = `${apiPrefix}${url.startsWith('/') ? url : `/${url}`}`
+
+  // 5.3 结构body参数，并处理body对应的数据
+  const { body } = fetchOptions
+  if (body) options.body = JSON.stringify(body)
+
+  // 5.4 发起fetch请求并处理流式事件响应
+  const response = await globalThis.fetch(urlWithPrefix, options as RequestInit)
+  return handleStream(response, onData)
+}
+
+const handleStream = (response: Response, onData: (data: { [key: string]: any }) => void) => {
+  // 1.检测网络请求是否正常
+  if (!response.ok) throw new Error('网络请求失败')
+
+  // 2.构建reader以及deocder
+  const reader = response.body?.getReader()
+  const decoder = new TextDecoder('utf-8')
+  let buffer = ''
+
+  // 3.构建read函数用于去读取数据
+  const read = () => {
+    let hasError = false
+    reader?.read().then((result: any) => {
+      if (result.done) return
+
+      buffer += decoder.decode(result.value, { stream: true })
+      const lines = buffer.split('\n')
+
+      let event = ''
+      let data = ''
+
+      try {
+        lines.forEach((line) => {
+          line = line.trim()
+          if (line.startsWith('event:')) {
+            event = line.slice(6).trim()
+          } else if (line.startsWith('data:')) {
+            data = line.slice(5).trim()
+          }
+
+          // 每个事件以空行结束，只有event和data同时存在，才表示一次流式事件的数据完整获取到了
+          if (line === '') {
+            if (event !== '' && data !== '') {
+              onData({
+                event: event,
+                data: JSON.parse(data),
+              })
+              event = ''
+              data = ''
+            }
+          }
+        })
+        buffer = lines.pop() || ''
+      } catch (e) {
+        hasError = true
+      }
+
+      if (!hasError) read()
+    })
+  }
+
+  // 4.调用read函数去执行获取对应的数据
+  read()
+}
+
+export const request = <T>(url: string, options = {}) => {
+  return baseFetch<T>(url, options)
+}
+
+export const get = <T>(url: string, options = {}) => {
+  return request<T>(url, Object.assign({}, options, { method: 'GET' }))
+}
+
+export const post = <T>(url: string, options = {}) => {
+  return request<T>(url, Object.assign({}, options, { method: 'POST' }))
+}
