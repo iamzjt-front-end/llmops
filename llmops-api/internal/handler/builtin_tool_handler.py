7a82b5cd7159142411faa22ade26828b58a988dd
j.tide <iamzjt@foxmail.com>
2026-09-15 18:14:24 +0800

    feat(handler): 实现内置工具处理器并接入服务

---
+++ llmops-api/internal/handler/builtin_tool_handler.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+
+from injector import inject
+
+from internal.service.builtin_tool_service import BuiltinToolService
+from pkg.response import success_json
+
+
+@inject
+@dataclass
+class BuiltinToolHandler:
+  """内置工具处理器"""
+
+  builtin_tool_service: BuiltinToolService
+
+  def get_builtin_tools(self):
+    """获取LLMOps所有内置提供商＋工具信息"""
+    builtin_tools = self.builtin_tool_service.get_builtin_tools()
+    return success_json(builtin_tools)
+
+  def get_provider_tool(self, provider_name: str, tool_name: str):
+    """根据传递的提供商名字＋工具名字，获取指定的工具"""
+    pass
