c09c8feab8a0994995f81e780e5427c95573aed5
j.tide <iamzjt@foxmail.com>
2026-09-16 18:00:17 +0800

    feat(handler): 实现自定义 API 工具增删改查接口

--- llmops-api/internal/handler/api_tool_handler.py
+++ llmops-api/internal/handler/api_tool_handler.py
@@ -1,10 +1,21 @@
 from dataclasses import dataclass
+from uuid import UUID
 
+from flask import request
 from injector import inject
-from schema.api_tool_schema import ValidateOpenAPISchema
-from service import APIToolService
 
-from pkg.response import success_message, validate_error_json
+from internal.schema.api_tool_schema import (
+  CreateApiToolReq,
+  GetApiToolProviderResp,
+  GetApiToolProvidersWithPageReq,
+  GetApiToolProvidersWithPageResp,
+  GetApiToolResp,
+  UpdateApiToolProviderReq,
+  ValidateOpenAPISchemaReq,
+)
+from internal.service import ApiToolService
+from pkg.paginator import PageModel
+from pkg.response import success_json, success_message, validate_error_json
 
 
 @inject
@@ -12,16 +23,72 @@ from pkg.response import success_message, validate_error_json
 class ApiToolHandler:
   """自定义API插件处理器"""
 
-  api_tool_service: APIToolService
+  api_tool_service: ApiToolService
 
-  def validate_openai_schema(self):
-    """校验传递的openai_schema字符串是否正确"""
-    # 1.提取前端的数据并校验
-    req = ValidateOpenAPISchema()
+  def get_api_tool_providers_with_page(self):
+    """获取API工具提供者列表信息，该接口支持分页"""
+    req = GetApiToolProvidersWithPageReq(request.args)
     if not req.validate():
       return validate_error_json(req.errors)
 
-    # 2.调用服务并解析传递的数据
-    self.api_tool_service.parser_openapi_schema(req.openapi_schema.data)
+    api_tool_providers, paginator = (
+      self.api_tool_service.get_api_tool_providers_with_page(req)
+    )
+
+    resp = GetApiToolProvidersWithPageResp(many=True)
+
+    return success_json(
+      PageModel(list=resp.dump(api_tool_providers), paginator=paginator)
+    )
+
+  def create_api_tool_provider(self):
+    """创建自定义API工具"""
+    req = CreateApiToolReq()
+    if not req.validate():
+      return validate_error_json(req.errors)
+
+    self.api_tool_service.create_api_tool(req)
+
+    return success_message('创建自定义API插件成功')
+
+  def update_api_tool_provider(self, provider_id: UUID):
+    """更新自定义API工具提供者信息"""
+    req = UpdateApiToolProviderReq()
+    if not req.validate():
+      return validate_error_json(req.errors)
+
+    self.api_tool_service.update_api_tool_provider(provider_id, req)
+
+    return success_message('更新自定义API插件成功')
+
+  def get_api_tool(self, provider_id: UUID, tool_name: str):
+    """根据传递的provider_id+tool_name获取工具的详情信息"""
+    api_tool = self.api_tool_service.get_api_tool(provider_id, tool_name)
+
+    resp = GetApiToolResp()
+
+    return success_json(resp.dump(api_tool))
+
+  def get_api_tool_provider(self, provider_id: UUID):
+    """根据传递的provider_id获取工具提供者的原始信息"""
+    api_tool_provider = self.api_tool_service.get_api_tool_provider(provider_id)
+
+    resp = GetApiToolProviderResp()
+
+    return success_json(resp.dump(api_tool_provider))
+
+  def delete_api_tool_provider(self, provider_id: UUID):
+    """根据传递的provider_id删除对应的工具提供者信息"""
+    self.api_tool_service.delete_api_tool_provider(provider_id)
+
+    return success_message('删除自定义API插件成功')
+
+  def validate_openapi_schema(self):
+    """校验传递的openapi_schema字符串是否正确"""
+    req = ValidateOpenAPISchemaReq()
+    if not req.validate():
+      return validate_error_json(req.errors)
+
+    self.api_tool_service.parse_openapi_schema(req.openapi_schema.data)
 
     return success_message('数据校验成功')
