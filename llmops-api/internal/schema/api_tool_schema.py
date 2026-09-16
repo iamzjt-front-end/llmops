dd92c6a6a5145a9da7bf240d4f5188f6e36994ec
j.tide <iamzjt@foxmail.com>
2026-09-16 16:18:27 +0800

    feat(schema): 新增 OpenAPI 规范校验请求表单

---
+++ llmops-api/internal/schema/api_tool_schema.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
+class ValidateOpenAPISchema(FlaskForm):
+  """校验OpenAPI规范字符串请求"""
+
+  openapi_schema = StringField(
+    'openapi_schema',
+    validators=[
+      DataRequired(message='openai_schema字符串不能为空'),
+    ],
+  )
