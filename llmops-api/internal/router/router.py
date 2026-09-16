e9aea091739a819151fa0fd557af9a7973baae6a
j.tide <iamzjt@foxmail.com>
2026-09-16 18:00:17 +0800

    feat(router): 注册自定义 API 工具增删改查路由

--- llmops-api/internal/router/router.py
+++ llmops-api/internal/router/router.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass
 
 from flask import Blueprint, Flask
-from handler import ApiToolHandler
 from injector import inject
 
-from internal.handler import AppHandler, BuiltinToolHandler
+from internal.handler import ApiToolHandler, AppHandler, BuiltinToolHandler
 
 
 @inject
@@ -78,9 +77,36 @@ class Router:
 
     # 4.自定义API插件模块
     bp.add_url_rule(
+      '/api-tools',
+      view_func=self.api_tool_handler.get_api_tool_providers_with_page,
+    )
+    bp.add_url_rule(
       '/api-tools/validate-openapi-schema',
       methods=['POST'],
-      view_func=self.api_tool_handler.validate_openai_schema,
+      view_func=self.api_tool_handler.validate_openapi_schema,
+    )
+    bp.add_url_rule(
+      '/api-tools',
+      methods=['POST'],
+      view_func=self.api_tool_handler.create_api_tool_provider,
+    )
+    bp.add_url_rule(
+      '/api-tools/<uuid:provider_id>',
+      view_func=self.api_tool_handler.get_api_tool_provider,
+    )
+    bp.add_url_rule(
+      '/api-tools/<uuid:provider_id>',
+      methods=['POST'],
+      view_func=self.api_tool_handler.update_api_tool_provider,
+    )
+    bp.add_url_rule(
+      '/api-tools/<uuid:provider_id>/tools/<string:tool_name>',
+      view_func=self.api_tool_handler.get_api_tool,
+    )
+    bp.add_url_rule(
+      '/api-tools/<uuid:provider_id>/delete',
+      methods=['POST'],
+      view_func=self.api_tool_handler.delete_api_tool_provider,
     )
 
     # 4.在应用上去注册蓝图
